20bc26f6e8373cd892b1d6052c1df3a965a408e4
Gabryel-777 <gabryeldasilvalemos@gmail.com>
2025-03-28 20:23:01 -0300

    calculando juros e montante

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿double Taxajuros,Capital,Tempo;
+
+double Montante;
+
+double JurosSimples;
+
+ Console.Clear();
+
+Console.Write("Capital [c] (R$)....:");
+Capital = Convert.ToDouble (Console.ReadLine());
+Thread.Sleep(1500);
+
+Console.Write("Taxa de juros [i] (%)..: ");
+ Taxajuros = Convert.ToDouble(Console.ReadLine());
+Thread.Sleep(1500);
+
+Console.Write ("Tempo [t] (meses)......: ");
+ Tempo = Convert.ToDouble(Console.ReadLine());
+Thread.Sleep(1500);
+
+JurosSimples = Capital * Taxajuros /100* Tempo;
+
+Montante = Capital + JurosSimples; 
+
+Console.WriteLine ($"Juros: {Capital} * {Taxajuros} * {Tempo} = {JurosSimples}");
+Thread.Sleep(2500);
+
+Console.WriteLine ($"Montante: {Capital} + {JurosSimples} = {Montante}");
